8c9e8832cf8c1642ca1f544f698e0d44d2c0408f
taichi <taichi.01gr14@gmail.com>
2023-05-18 08:30:35 +0900

    second commit

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,24 @@ import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     credentials: true,
     origin: ['http://localhost:3000'],
   });
   app.use(cookieParser());
-  await app.listen(3005);
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
